5fa3e321ee219beee9566feb7e7855809493951b
Nikita <nikevdokimov34@gmail.com>
2020-07-16 14:15:14 +0300

    feat: Добавить автоматическую загрузку карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
-let content = document.querySelector(".page");
-let popupWindow = document.querySelector(".popup");
+const content = document.querySelector(".page");
+const popupWindow = document.querySelector(".popup");
+
+/* Выбор списка с фото */
+
+const photoList = content.querySelector(".photo-grid__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
 
 /* Выбор кнопок */
-let editButton = content.querySelector(".profile__edit-button");
-let cancelButton = popupWindow.querySelector(".popup__close-button");
+const editButton = content.querySelector(".profile__edit-button");
+const cancelButton = popupWindow.querySelector(".popup__close-button");
 
 /* Выбор формы и её полей */ 
 let formElement = popupWindow.querySelector(".popup__form");
@@ -13,6 +19,10 @@ let popupJob = popupWindow.querySelector(".popup__input_type_about");
 /* Выбор имени пользователя и его рода деятельности */
 let profileName = content.querySelector(".profile__title");
 let profileJob = content.querySelector(".profile__subtitle");
+
+/* Переменная, в которой храним массивы с названиями городов и ссылками на фото*/
+
+
 
 /* Функция, которая проверяет, присвоен ли модальному окну модификатор opened. */
 
@@ -40,7 +50,54 @@ function formSubmitHandler(evt) {
   makeVisible(); /* Закрываем модалку */
 }
 
+/* Функция, которая загружает содержимое на страницу */
+
+function loadCards () {
+
+  const initialCards = [
+    {
+        name: 'Волгоград',
+        link: './images/photo-grid/volgograd.jpg'
+    },
+    {
+        name: 'Челябинск',
+        link: './images/photo-grid/chelyabinsk.jpg'
+    },
+    {
+        name: 'Карелия',
+        link: './images/photo-grid/karelia.jpg'
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: './images/photo-grid/saint-p.jpg'
+    },
+    {
+        name: 'Сочи',
+        link: './images/photo-grid/sochi.jpg'
+    },
+    {
+        name: 'Мурманск',
+        link: './images/photo-grid/murmansk.jpg'
+    }
+  ];
+
+initialCards.map((elem) => {
+  let newItem = cardTemplate.cloneNode(true);
+
+  newItem.querySelector('.card__image').src = elem.link;
+  newItem.querySelector('.card__image').alt = `На фото: ${elem.name}`;
+  newItem.querySelector('.card__heading').textContent = elem.name;
+
+  photoList.append(newItem)
+})
+ 
+}
+
+loadCards();
 
 editButton.addEventListener("click", makeVisible);
 cancelButton.addEventListener("click", makeVisible);
 formElement.addEventListener("submit", formSubmitHandler);
+
+
+
