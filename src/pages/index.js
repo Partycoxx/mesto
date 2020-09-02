0aa0354a0571919ef5a099ec07fe5e6fb00fa90d
Nikita <nikevdokimov34@gmail.com>
2020-09-02 13:11:14 +0300

    Незначительные правки

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {
   addButton,
   editButton,
 } from "../utils/constants.js";
+import Card from "../components/Card.js"
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
